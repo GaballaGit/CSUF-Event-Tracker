5ab2e78d3bf34de21782ab12e0a90462e1030b8b
Mark Gaballa <mgaballa@csu.fullerton.edu>
2025-12-03 16:33:14 -0800

    Put pages behind different directories
    I found working like this easy with svelte, so I want to do it here

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from '../components/navbar'
-import  Dashboard  from './pages/dashboard'
-import  Finder  from './pages/finder'
+import Dashboard from './pages/dashboard//dashboard'
+import Finder from './pages/finder/finder'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0)
 
 
-  return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/find" element={<Finder />} />
-      </Routes>
-    </>
-  )
+	return (
+		<>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Dashboard />} />
+				<Route path="/find" element={<Finder />} />
+			</Routes>
+		</>
+	)
 }
 
 export default App
