f6613709d2ab3c776db13c4b78165a1413958ca4
gaballa <lgaballal@yahoo.com>
2025-12-03 23:19:35 -0800

    Some updates to dashboard ui

--- components/dashevent.tsx
+++ components/dashevent.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 
 export default function DashboardEvent({ event }: { event: Event }) {
 	const [time, updateTime] = useState(0)
+	const [tdd, toggleDropDown] = useState(false)
 
 	useEffect(() => {
 		const id = setInterval(() => {
@@ -12,18 +13,33 @@ export default function DashboardEvent({ event }: { event: Event }) {
 		return () => clearInterval(id);
 	}, [])
 
+	const toggleDD = () => {
+		toggleDropDown(!tdd)
+	}
 
 	return (
 		<>
 			<div id="eventdash">
-				<div id="edtext">
-					<h1>{event.name}</h1>
-					<h2>Starts in: ${time}</h2>
-					<button id="dropbutton">%gt;</button>
+				<div id="defshow">
+					<div id="edtext">
+						<h1>{event.name}</h1>
+						<h2>Starts in: {time}</h2>
+						<button id="dropbutton" onClick={toggleDD}>&#62;</button>
+					</div>
+					<div id="edpic">
+						<img src={event.image} />
+					</div>
 				</div>
-				<div id="edpic">
-					<img src={event.image} />
-				</div>
+						<div id="dropdown" className={tdd ? "close" : "open"}>
+							<div id="dropdowntop">
+								<h3>Host: {event.host}</h3>
+								<h3>Location: {event.location}</h3>
+							</div>
+							<div id="dropdownbottom">
+								<h3>Description:</h3>
+								<p>{event.description}</p>
+							</div>
+						</div>
 			</div>
 		</>
 	)
--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,17 +1,17 @@
 import type { Event } from '../../../types/types'
 import DashboardEvent from '../../../components/dashevent'
-
+import './dashboard.css'
 interface FinderProps {
 	savedEvents: Event[];
 	setSavedEvents: (event: Event[]) => void;
 }
 
 // TODO: Implement the dashboard Page
-export default function Dashboard({ savedEvents, setSavedEvents }: FinderProps) {
+export default function Dashboard({ savedEvents, }: FinderProps) {
 
 	return (
 		<>
-			<h1>Dashboard</h1>
+			<h1><span id="dashheader">Dashboard</span></h1>
 			{savedEvents.map((event,) => (
 				<DashboardEvent event={event} />
 			))}
