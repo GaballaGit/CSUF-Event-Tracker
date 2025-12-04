c7bd062bd1f1e059ca5395231b786dc6bee84207
Mark Gaballa <mgaballa@csu.fullerton.edu>
2025-12-03 16:11:36 -0800

    Add everything back lol

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,10 @@
+
+// TODO: Implement the dashboard Page
+export default function Dashboard() {
+
+    return (
+        <>
+            <h1>Dashboard</h1>
+        </>
+    )
+}
---
+++ src/pages/finder.tsx
@@ -0,0 +1,10 @@
+
+// TODO: Implement the event finder page
+export default function Finder() {
+
+    return (
+        <>
+            <h1>Finder</h1>
+        </>
+    )
+}
