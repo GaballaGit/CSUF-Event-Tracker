34effecc9f34c58bf1e2e0a9523ec97ad312dcba
gaballa <lgaballal@yahoo.com>
2025-12-05 20:28:39 -0800

    Fix pathing

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,8 +6,8 @@ export default function Navbar() {
 			<h1>CSUF Event Tracker</h1>
 			<div id="navlinks">
 				<nav>
-					<Link to="/"><h2>Dashboard</h2></Link>
-					<Link to="/find"><h2>Find Event</h2></Link>
+					<Link to="/CSUF-Event-Tracker"><h2>Dashboard</h2></Link>
+					<Link to="/CSUF-Event-Tracker/find"><h2>Find Event</h2></Link>
 				</nav>
 			</div>
 		</div>
--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 			<Navbar />
 			<Routes>
 				<Route
-					path="/"
+					path="/CSUF-Event-Tracker"
 					element={
 						<Dashboard
 							savedEvents={savedEvents}
@@ -22,7 +22,7 @@ function App() {
 					}
 				/>
 				<Route
-					path="/find"
+					path="/CSUF-Event-Tracker/find"
 					element={
 						<Finder
 							savedEvents={savedEvents}
